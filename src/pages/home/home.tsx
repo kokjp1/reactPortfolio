d9e6c983acc7b59aa94d0ac2bcba158b28f8220f
kokjp1 <james.kok@hva.nl>
2025-05-15 23:18:30 +0200

    test

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -7,7 +7,7 @@ function Home() {
             <Hero />
             <section className="container mx-auto px-18">
                 <h2 className="text-3xl font-semibold">About Me</h2>
-                <p className="mt-4 text-lg">Welcome to my portfolio. I am a 19 year old Student at the Amsterdam University of Applied Sciences (AUAS/HvA). Currently I'm studying Communication & Multimedia Design (CMD). I am interested in design, technology, interfaces and programming. What you find here is a mix of school projects and personal work.</p>        
+                <p className="mt-4 text-lg">Welcome to my portfolio. I am a 19 year old Student at the Amsterdam University of Applied Sciences (AUAS/HvA). Currently I'm studying Communication & Multimedia Design (CMD). I am interested in design, technology, interfaces and programming. What you find here is a mix of school projects and personal work. TESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTEST</p>        
             </section>
             <section className="container mx-auto px-18">
                 <ToolTiles />
